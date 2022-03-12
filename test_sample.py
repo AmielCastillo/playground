35047aec86cbcb8857aaa659f33248cf1318ea4f
AmielCastillo <46759229+AmielCastillo@users.noreply.github.com>
2022-03-11 21:58:38 -0500

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -3,7 +3,7 @@ Test helper functions
 """
 
 import unittest
-from sample import increment_by_two, increment_by_three
+from sample import increment_by_two
 
 class TestSampleMethods(unittest.TestCase):
    """
@@ -14,4 +14,4 @@ class TestSampleMethods(unittest.TestCase):
    self.assertEqual(increment_by_two(3),5)
     
 if __name__ == '__main__':
-  unittest.main()
+   unittest.main()
