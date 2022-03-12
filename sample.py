c1aae0d27a95f469197e4eeb307cc984fe421cf0
AmielCastillo <46759229+AmielCastillo@users.noreply.github.com>
2022-03-11 21:49:27 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,3 +1,12 @@
-def foo(a):
-   return a + 2
+"""
+Helper functions live here
+"""
+
+def increment_by_two(var_to_increment):
+    """
+    This function increments input by two
+    :param car_to_increment: param to increment
+    :return: incremented variable
+    """
+    return var_to_increment + 2
   
