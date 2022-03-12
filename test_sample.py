a9e2878f275a687c92879ec94a7e976320ca0a15
AmielCastillo <46759229+AmielCastillo@users.noreply.github.com>
2022-03-11 22:01:18 -0500

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -6,12 +6,12 @@ import unittest
 from sample import increment_by_two
 
 class TestSampleMethods(unittest.TestCase):
-   """
-   Test harness
-   """
-   self.assertEqual(increment_by_two(-2),0)
-   self.assertEqual(increment_by_two(0),1)
-   self.assertEqual(increment_by_two(3),5)
+    """
+    Test harness
+    """
+    self.assertEqual(increment_by_two(-2),0)
+    self.assertEqual(increment_by_two(0),1)
+    self.assertEqual(increment_by_two(3),5)
     
 if __name__ == '__main__':
-   unittest.main()
+    unittest.main()
