448f4583d87f8211868e86578dd6ee49c20906dd
AmielCastillo <46759229+AmielCastillo@users.noreply.github.com>
2022-03-11 21:55:26 -0500

    Create test_sample.py

---
+++ test_sample.py
@@ -0,0 +1,17 @@
+"""
+Test helper functions
+"""
+
+import unittest
+from sample import increment_by_two, increment_by_three
+
+class TestSampleMethods(unittest.TestCase):
+   """
+   Test harness
+   """
+    self.assertEqual(increment_by_two(-2),0)
+    self.assertEqual(increment_by_two(0),1)
+    self.assertEqual(increment_by_two(3),5)
+    
+if __name__ == '__main__':
+  unittest.main()
