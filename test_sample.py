a9a0ffbe861572208d8eefe48f1afb9fb031d6ef
AmielCastillo <46759229+AmielCastillo@users.noreply.github.com>
2022-03-11 22:02:46 -0500

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -9,9 +9,9 @@ class TestSampleMethods(unittest.TestCase):
     """
     Test harness
     """
-    self.assertEqual(increment_by_two(-2),0)
-    self.assertEqual(increment_by_two(0),1)
-    self.assertEqual(increment_by_two(3),5)
-    
+    unittest.TestCase.assertEqual(increment_by_two(-2),0)
+    unittest.TestCase.assertEqual(increment_by_two(0),1)
+    unittest.TestCase.assertEqual(increment_by_two(3),5)
+
 if __name__ == '__main__':
     unittest.main()
